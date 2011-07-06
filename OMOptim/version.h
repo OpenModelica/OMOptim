32c670a792b3c5d04c4f9ee79224303eaa7cc486
Hubert Thieriot <hubert.thieriot@mines-paristech.fr>
2011-07-06 15:06:52 +0000

    - updated independent installer and version numbering
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@9428 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/version.h
+++ OMOptim/version.h
@@ -7,7 +7,7 @@ namespace Version
 {
  static const int MAJOR = 0;
  static const int MINOR = 9;
- static const int REVISION = 9416;
+ static const int REVISION = 9418;
  
  
  static QString version()
