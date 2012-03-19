c550360d915c338526404e051fcb3117e00e2001
Adeel Asghar <adeel.asghar@liu.se>
2012-03-19 18:41:56 +0000

    - Fixed OpenModelica_CODING_STYLE.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@11465 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/version.h
+++ OMOptim/version.h
@@ -5,15 +5,15 @@
 
 namespace Version
 {
- static const int MAJOR = 0;
- static const int MINOR = 9;
- static const int REVISION = 11437;
- 
- 
- static QString version()
- {
-	return QString::number(MAJOR)+QString::number(MINOR)+QString::number(REVISION);
- }
+static const int MAJOR = 0;
+static const int MINOR = 9;
+static const int REVISION = 11437;
+
+
+static QString version()
+{
+  return QString::number(MAJOR)+QString::number(MINOR)+QString::number(REVISION);
+}
 }
 
 #endif // VERSION_H
