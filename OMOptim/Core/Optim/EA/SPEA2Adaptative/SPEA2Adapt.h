11c425181e3fb48bb80f97c2aea90fd408656b96
Martin Sjölund <martin.sjolund@liu.se>
2012-03-26 09:24:30 +0000

    - Converted OMOptim/OMShell to UTF-8
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@11533 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/Core/Optim/EA/SPEA2Adaptative/SPEA2Adapt.h
+++ OMOptim/Core/Optim/EA/SPEA2Adaptative/SPEA2Adapt.h
@@ -3,8 +3,8 @@
  * This file is part of OpenModelica.
  *
  * Copyright (c) 1998-CurrentYear, Open Source Modelica Consortium (OSMC),
- * c/o Linköpings universitet, Department of Computer and Information Science,
- * SE-58183 Linköping, Sweden.
+ * c/o LinkÃ¶pings universitet, Department of Computer and Information Science,
+ * SE-58183 LinkÃ¶ping, Sweden.
  *
  * All rights reserved.
  *
