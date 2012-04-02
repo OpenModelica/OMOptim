3b945eb7f01304391089da560d4dfd644323220d
Hubert Thieriot <hubert.thieriot@mines-paristech.fr>
2012-04-02 09:27:43 +0000

    - changed version reader from tortoise svn
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@11626 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/build/UpdateRevision.bat
+++ OMOptim/build/UpdateRevision.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ./ ./version.tmpl ./version.txt
-"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ./ ./version.h.tmpl ../version.h
+"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ../ ./version.tmpl ./version.txt
+"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ../ ./version.h.tmpl ../version.h
