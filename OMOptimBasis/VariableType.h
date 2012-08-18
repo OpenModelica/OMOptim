041ea9089ca795a2a45f788bf1627e0830ac5db0
Martin Sjölund <martin.sjolund@liu.se>
2012-08-18 06:37:15 +0000

    - Detect people who cannot spell Linköping correctly
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@12583 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptimBasis/VariableType.h
+++ OMOptimBasis/VariableType.h
@@ -2,8 +2,8 @@
  * This file is part of OpenModelica.
  *
  * Copyright (c) 1998-CurrentYear, Open Source Modelica Consortium (OSMC),
- * c/o Linkpings universitet, Department of Computer and Information Science,
- * SE-58183 Linkping, Sweden.
+ * c/o Linköpings universitet, Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
  *
  * All rights reserved.
  *
