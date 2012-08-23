dfa6333cdde6f09153ff341a7af72c7ebdbd8835
Hubert Thieriot <hubert.thieriot@mines-paristech.fr>
2012-08-23 12:25:45 +0000

    -doc
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@12651 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/version.h
+++ OMOptim/version.h
@@ -7,7 +7,7 @@ namespace Version
 {
  static const int MAJOR = 0;
  static const int MINOR = 9;
- static const int REVISION = 12606;
+ static const int REVISION = 12619;
  
  
  static QString version()
