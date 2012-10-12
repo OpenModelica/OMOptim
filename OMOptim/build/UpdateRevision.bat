44914bf90c21bc6644cc2dfe26d2bd46bbc7b439
Adrian Pop <adrian.pop@liu.se>
2012-10-12 09:35:46 +0000

    - do not exit with error even if it did not work! - needed for the Hudson build.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@13331 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/build/UpdateRevision.bat
+++ OMOptim/build/UpdateRevision.bat
@@ -1,2 +1,4 @@
 "C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ../ ./version.tmpl ./version.txt
 "C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ../ ./version.h.tmpl ../version.h
+REM exit without error even if there is one!
+exit /b 0
