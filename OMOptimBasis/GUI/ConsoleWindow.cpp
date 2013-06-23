dc76b3bbb69c3bfe67893cff87455282f673ff09
Hubert Thieriot <hubert.thieriot@mines-paristech.fr>
2013-06-23 05:25:52 +0000

    - added missing files ConsoleWindow
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@16450 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ OMOptimBasis/GUI/ConsoleWindow.cpp
@@ -0,0 +1,119 @@
+// $Id: ConsoleWindow.cpp 16221 2013-06-05 08:47:46Z hubert.thieriot $
+/**
+  @file ConsoleWindow.h
+  @brief Comments for file documentation.
+  @author Hubert Thieriot, hubert.thieriot@mines-paristech.fr
+  Company : CEP - ARMINES (France)
+  http://www-cep.ensmp.fr/english/
+  @version
+*/
+
+#include "ConsoleWindow.h"
+#include <QtGui/QMessageBox>
+#include <QtGui/QTreeWidgetItem>
+#include <QtGui/QInputDialog>
+#include <QtGui/QTabBar>
+#include <QtGui/QDockWidget>
+#include <QSettings>
+#include <QFileSystemWatcher>
+#include "MOThreads.h"
+#include "ListInfo.h"
+#include <iostream>
+#include <QDebug>
+#include "GuiTools.h"
+#include "scriptparser.h"
+#include "Dialogs/HelpDlg.h"
+#include "ScriptTextDlg.h"
+
+namespace Ui
+{
+class ConsoleWindowClass;
+}
+
+
+ConsoleWindow::ConsoleWindow(ProjectBase* project,QStringList commands,QWidget *parent)
+    : QMainWindow(parent), _ui(new Ui::ConsoleWindowClass)
+{
+
+    this->resize(800, 600);
+    this->setStyleSheet("QMainWindow::separator {height:0;width:0}");
+
+    //************************
+    // Basic initialization
+    //************************
+    _ui->setupUi(this);
+    _project = project;
+    _commands = commands;
+
+    actualizeGuiFromProject();
+
+
+
+    initLog();
+}
+
+ConsoleWindow::~ConsoleWindow()
+{
+    delete _ui;
+    delete _logProxy;
+}
+
+
+void ConsoleWindow::initLog()
+{
+    // text log
+    _logProxy = new QSortFilterProxyModel();
+    _logProxy->setSourceModel(InfoSender::instance()->infosNormal());
+    _ui->listLog->setModel(_logProxy);
+    //_ui->listDebug->setModel(InfoSender::instance()->infosDebug());
+    //_ui->listOM->setModel(InfoSender::instance()->infosOM());
+    _ui->listLog->setColumnWidth(0,60);
+    //_ui->listDebug->setColumnWidth(0,60);
+    //_ui->listOM->setColumnWidth(0,60);
+
+    _ui->listScript->setModel(InfoSender::instance()->infosScript());
+    _ui->listScript->setColumnWidth(0,60);
+
+//    // log buttons
+//    connect(_ui->pushDispWarning,SIGNAL(toggled(bool)),this,SLOT(dispOnlyWarnings(bool)));
+//    connect(_ui->pushStayInLine,SIGNAL(toggled(bool)),this,SLOT(stayInLine(bool)));
+//    _ui->pushStayInLine->toggle();
+
+}
+
+//void ConsoleWindow::displayInfo(const Info & i)
+//{
+//    if(i.infoType==ListInfo::TASK)
+//    {
+//        setStatusBarText(i.infoMsg);
+//    }
+//}
+
+
+void ConsoleWindow::actualizeGuiFromProject()
+{
+    QString windowTitle = "OMOptim - "+_project->name();
+    if(!_project->isSaved()&& _project->isDefined())
+        windowTitle.append("*");
+    this->setWindowTitle(windowTitle);
+}
+
+int ConsoleWindow::launch()
+{
+    connect(this,SIGNAL(destroyed()),_project->scriptParser(),SLOT(stop()));
+
+    bool scriptOk = _project->scriptParser()->executeCommands(_commands);
+    if(scriptOk)
+        _result = 0;
+    else
+        _result = -1;
+
+    return _result;
+}
+
+void ConsoleWindow::closeEvent(QCloseEvent *event)
+{
+    qDebug("ConsoleWindow close event");
+   _project->scriptParser()->stop();
+   QMainWindow::closeEvent(event);
+}
---
+++ OMOptimBasis/GUI/ConsoleWindow.h
@@ -0,0 +1,87 @@
+// $Id: ConsoleWindow.h 16202 2013-06-04 07:28:54Z hubert.thieriot $
+/**
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Open Source Modelica Consortium (OSMC),
+ * c/o Linköpings universitet, Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3 LICENSE OR
+ * THIS OSMC PUBLIC LICENSE (OSMC-PL).
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE
+ * OF THE OSMC PUBLIC LICENSE OR THE GPL VERSION 3, ACCORDING TO RECIPIENTS CHOICE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from OSMC, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or
+ * http://www.openmodelica.org, and in the OpenModelica distribution.
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ * Main contributor 2010, Hubert Thierot, CEP - ARMINES (France)
+
+    @file ConsoleWindow.h
+    @brief Comments for file documentation.
+    @author Hubert Thieriot, hubert.thieriot@mines-paristech.fr
+    Company : CEP - ARMINES (France)
+    http://www-cep.ensmp.fr/english/
+    @version
+*/
+
+#ifndef ConsoleWindow_H
+#define ConsoleWindow_H
+
+#include <QtGui/QMainWindow>
+#include <QtGui/QProgressBar>
+#include <QtGui/QWidget>
+#include <QtGui/QFileDialog>
+#include <QtGui/QMessageBox>
+#include <QtCore/QMutex>
+#include "ProjectBase.h"
+#include "ListInfo.h"
+#include "ui_ConsoleWindow.h"
+#include <QtXml/QDomDocument>
+#include <QSortFilterProxyModel>
+
+namespace Ui
+{
+class ConsoleWindowClass;
+}
+
+class ConsoleWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ConsoleWindow(ProjectBase*,QStringList scriptCommands,QWidget *parent = 0);
+    ~ConsoleWindow();
+
+
+
+    void initLog();
+    void actualizeGuiFromProject();
+    int launch();
+
+    int _result;
+    void closeEvent(QCloseEvent *event);
+
+private :
+    ProjectBase *_project;
+    QStringList _commands;
+
+
+    //Gui
+    Ui::ConsoleWindowClass *_ui;
+    QSortFilterProxyModel* _logProxy;
+};
+
+#endif // ConsoleWindow_H
