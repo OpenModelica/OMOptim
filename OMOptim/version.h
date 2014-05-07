45cfe0cd5fa5eca532c92cb224edf70ec40879ce
Hudson User <openmodelica@ida.liu.se>
2014-05-07 13:02:00 +0000

    [Janitor mode] Fix whitespace
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@20438 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/version.h
+++ OMOptim/version.h
@@ -8,8 +8,8 @@ namespace Version
  static const int MAJOR = 0;
  static const int MINOR = 9;
  static const int REVISION = 20164;
- 
- 
+
+
  static QString version()
  {
    return QString::number(MAJOR)+QString::number(MINOR)+QString::number(REVISION);
