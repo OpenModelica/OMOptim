1969c604fe1a4209a77b2c9496987e207bf50219
Adeel Asghar <adeel.asghar@liu.se>
2014-05-21 20:53:56 +0000

    - Fixed OMOptim compilation. - Copy the libs to build/lib/omc instead of build/lib
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@20748 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- OMOptim/version.h
+++ OMOptim/version.h
@@ -7,9 +7,9 @@ namespace Version
 {
  static const int MAJOR = 0;
  static const int MINOR = 9;
- static const int REVISION = 20438;
-
-
+ static const int REVISION = 20741;
+ 
+ 
  static QString version()
  {
    return QString::number(MAJOR)+QString::number(MINOR)+QString::number(REVISION);
